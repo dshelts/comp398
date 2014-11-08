6cf78cf81f55d0821126eceb4e91e76d35419dba
Andrew Shelton <shelton_andrew@wheatoncollege.edu>
2014-11-08 10:27:20 -0500

    doesn't clear the collection yet err with .remove()

---
+++ hw14/populate.js
@@ -0,0 +1,29 @@
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://dshelly:abc123@ds047930.mongolab.com:47930/messaround", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+  else{
+  	console.log(err);
+  }
+
+	var collection = db.collection('hw14');
+	// Clearing out the collection
+	//collection.remove({});
+	var testArray = [];
+	for(i=0; i<2500; i++){
+		testArray.push({myKey: i })
+		console.log("adding "+i)
+	}
+	
+	
+	collection.insert(testArray, {w:1}, function(err, result) {});
+	console.log("got here")
+
+
+
+});
